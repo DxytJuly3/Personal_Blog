fb6ef3175e08a8776f0b97c889e05052d5825d80
DxytJuly3 <dxyt2002@163.com>
2023-04-08 19:17:53 +0800

    首次提交相关博客文章 2023-4-8

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `July.cc Blogs`;
+export const SITE_DESCRIPTION = '七月.cc 的个人博客';
+export const SITE_EMAIL = 'dxyt2002@163.com'
+export const SITE_NAME = 'julysblog.cn';
+export const SITE_URL = "https://julysblog.cn";
