6e6cf7b6449fcf73e43e7784f0368ccb918e33b3
DxytJuly3 <dxyt2002@163.com>
2023-04-09 12:19:20 +0800

    更改标签图测试

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `七月.cc Blogs`;
+export const SITE_TITLE = `July.cc Blogs`;
 export const SITE_DESCRIPTION = '七月.cc 的个人博客';
 export const SITE_EMAIL = 'dxyt2002@163.com'
 export const SITE_NAME = 'www.julysblog.cn';
