d350f9808ec07618f65eedb6b4daf6bb1d0ba18d
七月.cc <dxyt2002@163.com>
2023-06-29 17:16:20 +0800

    修改文章部分元素布局格式

--- public/static/js/initPost.js
+++ public/static/js/initPost.js
@@ -110,11 +110,11 @@ function syntaxHighlight() {
 
   var styleLight = document.createElement("link");
   styleLight.rel = "stylesheet";
-  styleLight.href = "//cdn.staticfile.org/highlight.js/11.7.0/styles/grayscale.min.css";
+  styleLight.href = "//cdn.staticfile.org/highlight.js/11.7.0/styles/arduino-light.min.css";
 
   var styleDark = document.createElement("link");
   styleDark.rel = "stylesheet";
-  styleDark.href = "//cdn.staticfile.org/highlight.js/11.7.0/styles/grayscale.min.css";
+  styleDark.href = "//cdn.staticfile.org/highlight.js/11.7.0/styles/arduino-light.min.css";
 
   if (document.querySelector("body").classList.contains("theme-dark")) {
     document.head.appendChild(styleDark);
